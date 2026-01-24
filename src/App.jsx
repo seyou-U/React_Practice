141d0de6cc6798d7e87c1ca6a5c448f6d1836a84
seyou-U <rikakorink54t@yahoo.co.jp>
2026-01-24 15:22:40 +0900

    [add] コンポーネントを分割した画面作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
 import './App.css'
-import { StrictModeLog } from './components/StrictModelog';
 import { Home } from './pages/Home'
+import { Header } from './Header';
+import { Footer } from './Footer';
 
+// Appは親コンポーネントであり画面の設計図
+// どの部品をの順番で並べるかについて整理している
 function App() {
   return (
-    <>
-      <StrictModeLog />
+    <div>
+      <Header />
       <Home />
-    </>
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/Footer.jsx
@@ -0,0 +1,3 @@
+export function Footer() {
+    return <small>© 2026 Sample Company</small>;
+}
