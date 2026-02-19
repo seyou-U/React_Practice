9087ee2cfcf42d808928550e75e7d794d40e4d6e
seyou-U <rikakorink54t@yahoo.co.jp>
2026-02-20 06:23:35 +0900

    [add] Suspenseを用いてローディング画面を追加する

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css';
-import RenderPropsDemo from './pages/RenderPropsDemo';
+import { ErrorBoundary } from 'react-error-boundary';
+import { AppFallback } from './components/Fallback/AppFallback';
+import { lazy, Suspense } from 'react';
+import { AppLoading } from './components/Fallback/AppLoading';
+
+const RootLayout = lazy(() => import("./routes/RootLayout"));
 
 function App() {
-  return <RenderPropsDemo/>;
+  return (
+    <ErrorBoundary
+      FallbackComponent={AppFallback}
+      onError={(error, info) => console.error(error, info)}
+    >
+      <Suspense fallback={<AppLoading />}>
+        <RootLayout />
+      </Suspense>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
